a840bd604eb99a1564782ae1f32142ee5c6ef6c9
Javier Quiles <javiquiles@hotmail.com>
2020-07-22 23:36:33 -0300

    Adding tableView at Home View

--- BestMovies/Home/View/HomeViewController.swift
+++ BestMovies/Home/View/HomeViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
     private let router = HomeRouter()
     private let viewModel = HomeViewModel()
 
@@ -17,6 +20,25 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
 
         viewModel.bind(view: self, router: router)
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
+}
+
+extension HomeViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") else { return UITableViewCell() }
+        return cell
+    }
+
+}
+
+extension HomeViewController: UITableViewDelegate {
+    
 }
