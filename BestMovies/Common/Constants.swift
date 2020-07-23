3a7ef43e8a6f3ca1b86718305f322c47d7bdabd4
Javier Quiles <javiquiles@hotmail.com>
2020-07-22 23:10:59 -0300

    reorder folder

---
+++ BestMovies/Common/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  BestMovies
+//
+//  Created by Javier Quiles on 22/07/2020.
+//  Copyright © 2020 Javier Quiles. All rights reserved.
+//
+
+struct Constants {
+    static let apiKey = "f441ecb7123dcbb5c41647da34f09cc1"
+
+    struct URL {
+        static let base = "https://api.themoviedb.org/3/"
+        static let images = "https://image.tmdb.org/t/p/w200"
+    }
+
+    struct Endpoints {
+        static let listPopularMovies = "movie/popular"
+        static let movieDetail = "movie"
+    }
+}
